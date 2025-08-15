8e2233207d828c06d843e7220285697226a06f8c
TwinkleMedia <hs0052264@gmail.com>
2025-08-15 20:51:40 +0530

    jh

--- dashboard.php
+++ dashboard.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Twinke / Admin Dashboard</title>
+    <link rel="stylesheet" href="./sidenavabar/sidenavbar.css">
 </head>
 <body>
 <?php  
